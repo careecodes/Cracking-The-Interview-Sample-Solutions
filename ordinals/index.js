2f3a7217a12280e65852d1b97503d5db71d7b67c
Jennifer Moore <jenniferplusplus@gmail.com>
2019-11-13 14:10:01 -0600

    added tests and fixed the solution for larger numbers

--- ordinals/index.js
+++ ordinals/index.js
@@ -1,13 +1,30 @@
+/***
+ * Important things to consider:
+ *
+ * What about non-english languages?
+ * What about numbers >2 digits
+ * What about teens (should all be 'th')
+ *
+ * For a more complete solution/extra credit:
+ *
+ * What about negative numbers
+ * What about non-integer numbers
+ * What about non-base 10 numbers
+ * How would you test it
+ */
+
 function toOrdinal(number) {
     const asString = String((+number).toFixed(0));
-    if(number >= 10 && number < 20)
+    const tens = asString.slice(-2);
+
+    if(tens >= 10 && tens < 20)
     {
         return asString + suffix['0'];
     }
 
     const finalDigit = asString.charAt(asString.length - 1);
 
-    return asString + suffix[finalDigit] || suffix['0'];
+    return asString + (suffix[finalDigit] !== undefined ? suffix[finalDigit] : suffix['0']);
 }
 
 const suffix = {
